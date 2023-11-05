a13c6095a06dab792c2d53c5ca7385e5266c6023
Michael Niekamp <michael.niekamp@umconnect.umt.edu>
2023-11-05 13:07:20 -0700

    added notification channel infrastructure

--- app/src/main/java/com/example/notif/HomeFragment.kt
+++ app/src/main/java/com/example/notif/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.notif
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -15,10 +20,12 @@ import com.example.notif.model.NotiViewModel
 class HomeFragment : Fragment() {
     private var binding : FragmentHomeBinding? = null
     private val sharedViewModel: NotiViewModel by activityViewModels()
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        createNotificationChannel()
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val button = view.findViewById<Button>(R.id.home_button)
@@ -26,5 +33,20 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeFragment_to_dataFragment)
         }
         return view
+    }
+
+    private fun scheduleNotiication(){
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(){
+        val name = "Noti Channel"
+        val desc = "Description of Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelID, name, importance)
+        channel.description = desc
+        val notificationManager = activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
